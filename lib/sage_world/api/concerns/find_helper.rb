03731909d0dfbd09bacd848d4dbed72b8c6ab403
Abhishek Kanojia <abhishek.kanojia@vinsol.com>
2018-12-12 17:16:22 +0530

    Add usage comment for helper method.

--- lib/sage_world/api/concerns/find_helper.rb
+++ lib/sage_world/api/concerns/find_helper.rb
@@ -3,7 +3,23 @@ module SageWorld
     module FindHelper
 
       module InstanceMethods
+        # SageWorld Api provides helper method on SageWorld::ResponseHandler
+        # which can return the key in nested hash or array and return its value.
+        #
+        # e.g response = SageWorld::Api::Product.search("mugs")
+        # Find in hash provides an easy way to do
+        # response.as_hash[:xml_data_stream_response][:search_results][:items][:item] => simplifies to
+        # response.find_in_hash(:item) => returns array of items.
 
+        # Similarly, for product
+        # product = SageWorld::Api::Product.new("id")
+        # response = product.details.as_hash
+        # {
+        #   xml_data_stream_response: {
+        #     product_details: { .. }
+        #   }
+        # }
+        #
         def find_in_hash(lookup_key, haystack = as_hash)
           if haystack.respond_to?(:key?) && haystack.key?(lookup_key)
             haystack[lookup_key]
@@ -13,7 +29,8 @@ module SageWorld
             data
           end
         end
-
+        # Finding in Xml
+        # response.find_in_xml("Item") => Array of items or nil.
         def find_in_xml(lookup_key, haystack = as_xml)
           haystack.xpath("//#{lookup_key}")
         end
