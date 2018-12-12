6dc791def0e05e5c4a4ee1b2741abf39f43b53c9
Abhishek Kanojia <abhishek.kanojia@vinsol.com>
2018-12-12 17:00:38 +0530

    Complete supplier list usage comments.

--- lib/sage_world/api/supplier_list.rb
+++ lib/sage_world/api/supplier_list.rb
@@ -2,6 +2,25 @@ module SageWorld
   module Api
     class SupplierList < SageWorld::Api::Base
 
+      # Example Usage:
+      #
+      # response = SageWorld::Api::SupplierList.get()
+        # response.as_hash => list as hash
+        # response.as_xml => list as xml
+
+      #
+      # Options
+      #
+        # Sorting
+        # Supplier list can be sorted by sageid, company or line
+        # e.g response = SageWorld::Api::SupplierList.get({ sort: "sageid" })
+        # sort by company response = SageWorld::Api::SupplierList.get({ sort: "company" })
+        # sort by line response = SageWorld::Api::SupplierList.get({ sort: "line" })
+
+      # AllLines => if set to 1 will return AllLines with every supplier
+      # e.g response = SageWorld::Api::SupplierList.get({ alllines: 1 })
+      #
+
       def self.get(params = {})
         if @existing_params == params
           @response
